6ae088cdb59e62d60db4e1b3189950e40355bdc2
malit mohamut <malitmohamud@gmai.com>
2023-05-03 11:44:26 +0300

    done

---
+++ example2-logic.js
@@ -0,0 +1,14 @@
+// javascript
+//Example:
+// if age is over 18 and hasLicense is true and hasCar is true console you can drive
+// else go walk!
+
+let hasCar = true
+let hasLicense = false
+let age = 21
+
+if (age > 18 && hasLicense===true && hasCar===true) {
+  console.log("You can drive!");
+} else {
+  console.log("Go walk!");
+}
