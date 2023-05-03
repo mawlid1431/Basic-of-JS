4bb0b4a21eb783ae44bebc6cd57b5446d19f86d9
malit mohamut <malitmohamud@gmai.com>
2023-05-03 11:08:09 +0300

    second day of the course that is push

---
+++ example-logico.js
@@ -0,0 +1,10 @@
+// console.log('Hello live examples');
+let age=22
+if (age<10){
+    console.log("You are chuld");
+} else if(age>10 &&  age <20){
+    console.log("You are teenager");
+}else if (age <7 || age ==9){ console.log(" you are a baby");}
+else if (age>20){
+    console.log("you are adult person");
+}
---
+++ logico.js
@@ -0,0 +1,29 @@
+
+// In JavaScript, there are three main logic operators:
+// && (logical AND): returns true if both operands are true, otherwis
+//  returns false.
+// || (logical OR): returns true if at least one of the operands is true,
+//  otherwise returns false.
+// ! (logical NOT): reverses the logical state of its operand. 
+// If a condition is true, then ! will make it false, 
+// and if a condition is false, then ! will make it true.
+// These logic operators are often used in conditional 
+// statements and loops to control the flow of a program based on certain conditions.
+
+
+// console.log(10>5);
+// console.log(20==20);
+// console.log(20<110);
+console.log(10>5 && 20<110);
+
+// mar walipa aad rabto in aad isticmaasho &&=AND  WAA IN LABADA CONDITION AY TRUE YIHIN  BOTH OF THEM
+
+console.log(20>110 || 20<35);
+
+
+
+// or ==|| hadii mid ka mid ah uu true yhy waxa kuso baxya result true ah 
+// hadii alabadaba ya been yihiin aya aad  result sax ah heli kartaa
+
+//not=! hadii aan ka horaysiiyo result will be false
+console.log(!20==20)
